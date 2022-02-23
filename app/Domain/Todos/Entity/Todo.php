439bb2bef083c5d656b5d311f416b3bc34a21562
fbo <fboudo@gmail.com>
2022-02-23 22:45:29 +0100

    Ajout test unitaire de l'entité Todo

--- app/Domain/Todos/Entity/Todo.php
+++ app/Domain/Todos/Entity/Todo.php
@@ -6,39 +6,17 @@ use Carbon\Carbon;
 
 class Todo
 {
-    /**
-     * @var string
-     */
-    private $id;
+    private string $id;
 
-    /**
-     * @var bool
-     */
-    private $checked;
+    private bool $checked;
 
-    /**
-     * @var string
-     */
-    private $text;
+    private string $text;
 
-    /**
-     * @var Carbon
-     */
-    private $createdAt;
+    private Carbon $createdAt;
 
-    /**
-     * @var Carbon
-     */
-    private $updatedAt;
+    private ?Carbon $updatedAt;
 
-    /**
-     * @param string $id
-     * @param bool $checked
-     * @param string $text
-     * @param Carbon $createdAt
-     * @param Carbon $updatedAt
-     */
-    public function __construct(string $id, bool $checked, string $text, Carbon $createdAt, Carbon $updatedAt)
+    public function __construct(string $id, bool $checked, string $text, Carbon $createdAt, Carbon $updatedAt = null)
     {
         $this->id = $id;
         $this->checked = $checked;
@@ -47,65 +25,41 @@ class Todo
         $this->updatedAt = $updatedAt;
     }
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @return Carbon
-     */
     public function getCreatedAt(): Carbon
     {
         return $this->createdAt;
     }
 
-    /**
-     * @return bool
-     */
     public function isChecked(): bool
     {
         return $this->checked;
     }
 
-    /**
-     * @param bool $checked
-     */
     public function setChecked(bool $checked): void
     {
         $this->checked = $checked;
     }
 
-    /**
-     * @return string
-     */
     public function getText(): string
     {
         return $this->text;
     }
 
-    /**
-     * @param string $text
-     */
     public function setText(string $text): void
     {
         $this->text = $text;
     }
 
-    /**
-     * @return Carbon
-     */
-    public function getUpdatedAt(): Carbon
+    public function getUpdatedAt(): ?Carbon
     {
         return $this->updatedAt;
     }
 
-    /**
-     * @param Carbon $updatedAt
-     */
     public function setUpdatedAt(Carbon $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
---
+++ tests/Unit/Entity/TodoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Entity;
+
+use App\Domain\Todos\Entity\Todo;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\TestCase;
+
+class TodoTest extends TestCase
+{
+    private Todo $todoTest;
+
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        $this->todoTest = new Todo(Str::uuid(),false,Str::random(30),Carbon::now(), Carbon::now());
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testIsChecked(): void
+    {
+        $this->todoTest->setChecked(true);
+        $this->assertTrue($this->todoTest->isChecked());
+
+        $this->todoTest->setChecked(false);
+        $this->assertFalse($this->todoTest->isChecked());
+    }
+
+    public function testText(): void
+    {
+        $text = Str::random(20);
+        $this->todoTest->setText($text);
+        $this->assertEquals($text,$this->todoTest->getText());
+    }
+
+    public function testCreatedAt(): void
+    {
+        $this->assertInstanceOf(Carbon::class,$this->todoTest->getCreatedAt());
+    }
+
+    public function testUpdatedAt(): void
+    {
+        $this->assertInstanceOf(Carbon::class,$this->todoTest->getUpdatedAt());
+    }
+}
